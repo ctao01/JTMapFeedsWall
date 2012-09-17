fe2bef8eabc9c47387f49aa46681a48fa02894e5
joycytao <ctao01.poly@gmail.com>
2012-09-17 12:13:31 -0500

    Initial Commit

---
+++ JTMapFeedsWaall/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JTMapFeedsWaall
+//
+//  Created by Joy Tao on 9/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
